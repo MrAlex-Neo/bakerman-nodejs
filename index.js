7b0fcd3b6f4db418d3a459cfc6317d89ce190599
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-08-14 13:24:26 +0500

    fix cors

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000
-const corsOptions = {
-    allowedHeaders: ['Content-Type'], // Добавьте сюда другие разрешенные заголовки, если это необходимо
-  };
-  
+
 const app = express()
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/auth', authRouter)
-app.use(cors(corsOptions));
+app.use(cors());
 
 const start = async () => {
     try {
